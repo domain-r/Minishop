e9d91361cad2f96777d215ba3ae37e28b0a1d6f4
Ruxue <jiruxuen@gmail.com>
2026-02-09 20:20:15 +0700

    Delete cart.js

--- cart.js
+++
@@ -1,7 +0,0 @@
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-function addToCart(id=1){
-  cart.push(id);
-  localStorage.setItem('cart', JSON.stringify(cart));
-  alert('Masuk keranjang');
-}
